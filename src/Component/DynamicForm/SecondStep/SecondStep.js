e0d10957f6fc7b8508e4076e8dba0ae3cd352965
Manuel Quintero <maluz1123@gmai.com>
2022-09-20 17:32:31 -0500

    feat: add step two and three

--- src/Component/DynamicForm/SecondStep/SecondStep.js
+++ src/Component/DynamicForm/SecondStep/SecondStep.js
@@ -1,5 +1,53 @@
-import "./SecondStep.css";
+import { useState } from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import Select from "react-select";
+
+const options = [
+  { id: 1, value: "football", label: "Football ⚽" },
+  { id: 2, value: "computer", label: "Computer 💻" },
+  { id: 3, value: "play", label: "Play 🎮" },
+  { id: 4, value: "music", label: "Play Music 🎼" },
+  { id: 5, value: "photo", label: "Photography 📷" },
+  { id: 6, value: "swim", label: "Swim 🥽" },
+  { id: 7, value: "run", label: "Run 👟" },
+];
 
 export default function SecondStep() {
-  return <div>SecondStep</div>;
+  const [selectOption, setSelectOption] = useState(null);
+
+  const hobbiesSer = JSON.stringify(selectOption);
+  localStorage.setItem("hobbies", hobbiesSer);
+
+  const onChangeAdditionalHobbie = (e) => {
+    const sport = e.target.value;
+    localStorage.setItem("adittionalHobbie", sport);
+  };
+  return (
+    <div>
+      <h2>Select your favourite sport</h2>
+      <h4>Tell me about your hobbies</h4>
+      <Container className="p-5 text-center">
+        <Row className="justify-content-md-center">
+          <Col lg="7">
+            <h5>Select your favourite sport</h5>
+            <Select
+              defaultValue={selectOption}
+              onChange={setSelectOption}
+              options={options}
+              isSearchable
+              isMulti
+            />
+          </Col>
+          <Col lg="7" className="mt-4">
+            <h5>Do you want to add more?</h5>
+            <Form.Control
+              type="text"
+              placeholder="Karate"
+              onChange={(e) => onChangeAdditionalHobbie(e)}
+            ></Form.Control>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
--- src/Component/DynamicForm/ThridStep/ThridStep.js
+++ src/Component/DynamicForm/ThridStep/ThridStep.js
@@ -1,3 +1,33 @@
+import { Col, Container, FloatingLabel, Form, Row } from "react-bootstrap";
+import "./ThridStep.css";
+
 export default function ThridStep() {
-  return <div>ThridStep</div>;
+  return (
+    <div>
+      <h2>Third Step</h2>
+      <h4>Do you want to add any comment?</h4>
+      <Container className="p-5 text-cemter">
+        <Row className="justify-content-md-center">
+          <Col lg="10">
+            <FloatingLabel
+              controlId="floatingTextarea"
+              label="Comments"
+              className="mb-3"
+            >
+              <Form.Control
+                as="textarea"
+                placeholder="Leave a comment"
+                onChange={(e) => console.log(e.targe.value)}
+              />
+            </FloatingLabel>
+          </Col>
+        </Row>
+        <div className="send-form">
+          <p className="result p-3" onClick={() => console.log("View result")}>
+            View result 🙌
+          </p>
+        </div>
+      </Container>
+    </div>
+  );
 }
